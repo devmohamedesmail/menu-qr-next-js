71e83d786ec4503ea352077265d514585577afbc
Mohamed Esmail <dev.mohamed.esmail@gmail.com>
2025-03-13 23:02:01 +0400

    Fix Dashboard Page

--- app/pages/admin/dashboard/page.js
+++ app/pages/admin/dashboard/page.js
@@ -1,15 +1,15 @@
-import AdminSidebar from '@/app/components/AdminSidebar/AdminSidebar'
-import HeaderAdmin from '@/app/components/HeaderAdmin/HeaderAdmin'
-import CustomDashboardIcon from '@/app/customcomponents/CustomDashboardIcon'
-import Link from 'next/link'
+'use client'
 import React from 'react'
-
+import HeaderAdmin from '@/app/components/HeaderAdmin/HeaderAdmin'
+import AdminSidebar from '@/app/components/AdminSidebar/AdminSidebar'
 export default function page() {
     return (
         <div className='container m-auto px-5'>
-            {/* <HeaderAdmin /> */}
+            <HeaderAdmin />
+            
             <div className='grid grid-cols-12'>
-               {/* <AdminSidebar /> */}
+               <AdminSidebar />
+               
                 <div className='col-span-12 lg:col-span-7'>
                     <h3 className='text-white text-center font-bold'>preview</h3>
                 </div>
